22842e23c4a25db1413b6e07e965cf0273124a33
Maxwell <maxwellluokl@gmail.com>
2024-04-03 21:05:32 +0800

    Replace the parameters of the CRUD functions with self.data & self.conditions

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,24 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2 import sql
 from psycopg2.extensions import connection as Connection
 
+
 class BaseModel:
 
     table_name = ""
 
     def __init__(self, db_connection: Connection):
         self.connection = db_connection
+        self.data = None
+        self.conditions = None
 
-    def table(self, table_name, table_definition):
+    def table(self, table_definition):
 
         try:
             query = sql.SQL("CREATE TABLE IF NOT EXISTS {name} ({definition});").format(
-                name=sql.Identifier(table_name),
+                name=sql.Identifier(self.table_name),
                 definition=sql.SQL(table_definition)
             )
+
             with self.connection.cursor() as cursor:
                 cursor.execute(query)
                 self.connection.commit()
@@ -24,43 +28,59 @@ class BaseModel:
         except Exception as err:
             print(f'Exception : {err}')
 
-    def create(self, data):
+    def set_data(self, **kwargs):
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                raise AttributeError(f'{key} is not valid attributes of {type(self).__name__}')
 
-        columns = data.keys()
-        values = tuple(data.values())
+        self.data = {key: value for key, value in self.__dict__.items()
+                     if value is not None and key not in ["connection", "data", "conditions"]}
+        return self.data
+
+    def set_conditions(self, **kwargs):
+
+        self.conditions = {key: value for key, value in kwargs.items() if hasattr(self, key)}
+        return self.conditions
+
+    def create(self):
+
+        columns = self.data.keys()
+        values = tuple(self.data.values())
 
         query = sql.SQL("INSERT INTO {table} ({fields}) VALUES ({placeholders}) RETURNING id;").format(
             table=sql.Identifier(self.table_name),
             fields=sql.SQL(', ').join(map(sql.Identifier, columns)),
             placeholders=sql.SQL(', ').join(sql.Placeholder() * len(values))
         )
+
         with self.connection.cursor() as cursor:
             cursor.execute(query, values)
             self.connection.commit()
             return cursor.fetchone()[0]
 
-    def read(self, conditions=None):
+    def read(self):
 
         where_clause = sql.SQL("WHERE {}").format(sql.SQL(' AND ').join(
-            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in conditions)
-        ) if conditions else sql.SQL('')
+            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in self.conditions)
+        ) if self.conditions else sql.SQL('')
 
         query = sql.SQL("SELECT * FROM {table} {where};").format(
             table=sql.Identifier(self.table_name),
             where=where_clause
         )
         with self.connection.cursor as cursor:
-            cursor.execute(query, tuple(conditions.values()) if conditions else ())
+            cursor.execute(query, tuple(self.conditions.values()) if self.conditions else ())
             return cursor.fetchall()
 
-
-    def update(self, conditions, data):
+    def update(self):
         set_clause = sql.SQL(', ').join(
-            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in data.keys()
+            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in self.data.keys()
         )
 
         where_clause = sql.SQL(' AND ').join(
-            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in conditions.keys()
+            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in self.conditions.keys()
         )
 
         query = sql.SQL("UPDATE {table} SET {set} WHERE {where};").format(
@@ -70,13 +90,13 @@ class BaseModel:
         )
 
         with self.connection.cursor() as cursor:
-            cursor.execute(query, tuple(data.values()) + tuple(conditions.values()))
+            cursor.execute(query, tuple(self.data.values()) + tuple(self.conditions.values()))
             self.connection.commit()
             return cursor.rowcount
 
-    def delete(self, conditions):
+    def delete(self):
         where_clause = sql.SQL(' AND ').join(
-            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in conditions.keys()
+            sql.Composed([sql.Identifier(key), sql.SQL('='), sql.Placeholder()]) for key in self.conditions.keys()
         )
 
         query = sql.SQL("DELETE FROM {table} WHERE {where};").format(
@@ -85,7 +105,7 @@ class BaseModel:
         )
 
         with self.connection.cursor() as cursor:
-            cursor.execute(query, tuple(conditions.values()))
+            cursor.execute(query, tuple(self.conditions.values()))
             self.connection.commit()
             return cursor.rowcount
 
