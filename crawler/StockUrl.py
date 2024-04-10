461b76e6c7e2ac4766e5d27272afe038404df66d
Maxwell <maxwellluokl@gmail.com>
2024-04-10 16:27:42 +0800

    [refactor] Remove outdated files

--- crawler/StockUrl.py
+++
@@ -1,7 +0,0 @@
-
-
-class StockUrl():
-    def __init__(self):
-        self.stock_number = ''
-    def GetDay(self, stock_number, date):
-        return 'https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=' + date + '&stockNo=' + stock_number
