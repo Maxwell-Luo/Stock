620d0cfdd0a35ad381aa633af03cf712a8665103
Maxwell <maxwellluokl@gmail.com>
2024-04-11 13:06:25 +0800

    [feat] Remove the old code and launch the crawler

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from models.base_model import BaseModel
-from models.postgre import Pg
+from crawler.crawler import Crawler
 
-pg = Pg()
-
-pg.check_postgre_status()
-pg.initial_database()
-exit(1)
-crawler = WebCrawler("2303")
-day_data = crawler.GetStockDayData("20240301")
-
-crawler.ParseStockDayData(day_data)
+crawler = Crawler()
+crawler.start()
