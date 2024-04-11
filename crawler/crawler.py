ae0d38b73b99c6a4ea30b7b6059ac99763c44f5f
Maxwell <maxwellluokl@gmail.com>
2024-04-11 13:25:58 +0800

    [feat] Run DailyTransactionSpider

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,13 +2,15 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from .spiders.company_info_spider import CompanyInfoSpider
+from .spiders.daily_transaction_spider import DailyTransactionSpider
+
 
 class Crawler:
 
-    def start(self) -> None:
+    def start(self):
         settings = get_project_settings()
         process = CrawlerProcess(settings=settings)
-        process.crawl(CompanyInfoSpider)
+        process.crawl(DailyTransactionSpider)
 
         process.start()
 
