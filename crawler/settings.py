d3451b9c4e7aa88a22149b71f74a2d7acaedc9db
Maxwell <maxwellluokl@gmail.com>
2024-04-14 18:06:15 +0800

    [config] Change log level

--- crawler/settings.py
+++ crawler/settings.py
@@ -20,7 +20,7 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = True
 
 # Log level
-LOG_LEVEL = "INFO"
+LOG_LEVEL = "ERROR"
 LOG_FILE = "logs/scrapy.log"
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
